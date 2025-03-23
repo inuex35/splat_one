6853f4f7e79d9a66639fbb95553aa9c02ff82d02
inuex35 <129066540+inuex35@users.noreply.github.com>
2025-03-23 23:50:28 +0900

    Add tabs package initialization module

---
+++ app/tabs/__init__.py
@@ -0,0 +1,9 @@
+# tabs package initialization
+from app.tabs.images_tab import ImagesTab
+
+# Import other tab classes as they are implemented
+# from app.tabs.masks_tab import MasksTab
+# from app.tabs.features_tab import FeaturesTab
+# from app.tabs.matching_tab import MatchingTab
+# from app.tabs.reconstruct_tab import ReconstructTab
+# from app.tabs.gsplat_tab import GsplatTab
